6ce2fd9b08f20fcc07b317b7e2c63d86d3006c51
yoora <espoir3045@gmail.com>
2025-11-22 23:25:28 +0900

    [251122] 수정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base:'/react-project3-pages/',
+  base:'./',
   plugins: [react()],
   optimizeDeps: {
     exclude: ["msw"],
